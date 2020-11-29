b53ddfca1a76373ba8c02c6b4f38638d644df77a
yimers <73725124+yimers@users.noreply.github.com>
2020-11-29 16:59:08 -0500

    this is medical bill statement

---
+++ src/billingproject/medicalbill.java
@@ -0,0 +1,8 @@
+package billingproject;
+
+public class medicalbill {
+    public static void main(String[] args) {
+        System.out.println("this is bill one for ekg");
+        System.out.println("this is lab bill");
+    }
+}
